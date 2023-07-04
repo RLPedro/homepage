77aa138a56898b2a78f6baade16056d7b25728f1
Ricardo Leitão Pedro <ricardoleitaopedro@gmail.com>
2023-07-04 14:39:49 +0200

    added blog post

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,7 +6,60 @@ const Blog = () => {
     <div className="content blog">
       <p className="blog__date">30 June 2023</p>
       <p className="blog__title">CLASSICAL MUSIC IS DEAD</p>
-      <p>lorem ipsum</p>
+      <p>
+        So.. Here I am writing for the first time in this format. I guess we still call it a blog.<br/>
+        In all honesty, it's more of a help to myself to get ideas in order and exorcise things. Still, if it helps or is of interest to anybody else, all the better!<br/>
+        <br/>
+        I'm a (classical) musician, or so I've called myself since I'm legally an adult. I started studying music “late”, according to tradition. I was “already” thirteen when I first stepped into the small music school which would be a sort of second home to me throughout my teens. I was practically eighteen(!) when I picked up the lute, which would be my main instrument from then on.<br/>
+        When I say 'from then on' I mean that from that moment on I was fully decided on becoming a professional classical musician, with all that entails:<br/>
+        - buying private lessons to prepare for college<br/>
+        - getting into local college for a first degree<br/>
+        - moving on for more degrees at increasing reputed colleges abroad with increasingly reputed teachers<br/>
+        - repeat; all the while auditioning for everything resembling opportunities (financially-wise, or most commonly, in the form of future hopes) both in Portugal and abroad.<br/>
+        <br/>
+        This is a pretty common path to follow as young aspiring musician of classical formation, at least in the context of Southern European countries. Portugal, Spain, Italy, etc, even though they all have (and have had for decades) residing international teaching musicians of the highest quality and reputation, they often lack in logistic and financial support that give their more ambitious students a sense of having the full range of possibilities in building their career. The reasons for these are manifold, mostly streaming from a cultural issue, and not really the point of my text here.<br/>
+        <br/>
+        My main point is rather connected to those factors which are not that obviously part of a (classical) musician's road into becoming a full-on professional. Or, more accurately, the factors that are not told nor exposed to us as young dreamy students, which means that unless we're born into a musical family we have no way to know of their existence. I certainly wasn't, and even when actually experiencing them personally much later on, it took me years and (what today I recognize as) a full burn-out to finally look at them in the face and see them for what they were.<br/>
+        <br/>
+        This is a somewhat complex matter, but I'll resume it like this:<br/>
+        CLASSICAL MUSIC IS NOT RELEVANT ANYMORE AS AN ART FORM.<br/>
+        <br/>
+        This is polemic (hopefully(!), otherwise it will just strengthen my point), so I'll try and explain what I mean by that.<br/>
+        <br/>
+        For those not so acquainted with the concert programmes of classical music venues, they don't differ that much all over the world. Some are more welcoming to the general public and will have mainstream or jazz acts alongside their local orchestras and choirs, others are more adventurous and include a couple contemporary music concerts in their season.<br/>
+        Regardless, the strategy is pretty much the same overall where the bulk remains music from a couple dozen (dead) composers performed by around ten different groups (including the local ones) directed by an equal number of the hype conductors of the time (themselves made out of one or two young rising stars plus a bunch of highly established authorities with an average age of 65). Obviously I'm not aiming at any precise statistics here, but rather at making a point through caricature and generalization.<br/>
+        <br/>
+        This is, in my view, a problem in itself, but it comes with a bunch of others resulting or otherwise leading to it. The education we receive as classical musicians is directed towards and leading into this problem, for one.<br/>
+        The borderline is that classical music has become for the most part a sort of museum-circus-live-entertainment. One for which we either work for decades to be able to perform in the fashion of such and such or otherwise go to great lengths to come off as new and exciting in the hope that it will make us stand out and finally get a place for ourselves in what has been for decades a completely sold-out space.<br/>
+        <br/>
+        You see, while still among the living, the dead composers we so much appreciate today were writing music of their own age, for their own time, to be heard by the (living) people around them. They studied the old masters, sure, even performed their music every now and again, but mostly were concerned with their own music and audiences, naturally.<br/>
+        We musicians of today though, play mostly their music (and very little music by living composers), go on stage dressed in a fashion still resembling formal dinner attire of the past century, abide to the performance rituals practised then and for the most part play in the same venues or similar ones.<br/>
+        On the other hand, we barely support living composers (specially from the younger generations) and most attempts to bring classical music outside its age-old habitat of historical concert venues and historical composers is either done in the context of a somewhat superficial attempt at reaching younger audiences or met with great resistance.<br/>
+        <br/>
+        In the meantime, musicians go through decades of training, travelling and living far away from family and friends, spending money they don't really have on instruments and materials while enduring daily hard-work and sacrifice. Only to be met at some point with the harsh truth on the few possibilities shaping their professional horizon:<br/>
+        continue to strive for the foreseeable future the same way they did until now (i.e. gigging, private teaching, auditioning), without any financial nor career stability in sight<br/>
+        dedicate most of your time to teaching (not that stable income-wise either, unless in the context of a university, and still..) and continue to perform when time allows<br/>
+        have a side job unrelated to music or performing, usually with much higher stability and income while also performing whenever possible<br/>
+        <br/>
+        Of course there are exceptions to these. Some musicians work very hard and eventually receive their moment in the Sun: they finally get that position in a renowned orchestra or win a competition which sets them going up and onwards in their career all of a sudden.<br/>
+        Nevertheless, these happen very rarely and, sadly, (way) more often than not, the success of a musician in today's age is mostly related to factors which have nothing to do with his or her actual quality as a musician. Networking and useful friendships are usually the most important assets to have in order to strive (assuming of course that you also have acceptable skills as a performer / teacher).<br/>
+        <br/>
+        You could say that it has always been so and it's the same in other professions. Sure.<br/>
+        Still, the market place in classical music is such a narrow space that you can barely call it a market, as in, a place where people are hired for a particular job by different employers on a frequent basis. Also, maybe contrarily to common opinion, I do believe that classical music (or perhaps the art world in general) lives even more out of favouritism and fabricated hype of particular personae than most professional environments.<br/>
+        To be sure, this is not in favour of the actual performers (even the current hyped ones, who can stop being so at a moment's notice), but rather to the benefit of old-established institutions in the cultural and political milieu which are interested in the refined look it gives them, among other benefits I can only imagine.<br/>
+        I can't explain it in any other way since classical music in its current shape doesn't in any way generate revenues that are worth any considerable investment, whether in education (virtually all music faculties generate debt rather than profit), ticket sales (most festivals and seasons live to a great extent out of government support and cultural grants), or worse yet, record sales (records these days are little more than entries in a musician's curriculum).<br/>
+        <br/>
+        I currently work as a full-time software engineer, so I even have an actual point of comparison now. But more on that in the upcoming second part to this post.<br/>
+        <br/>
+        Anyway, I hope by now my point is clearer.<br/>
+        Which, to be sure, is not to diminish the sublime composers and performers of the past (to me personally, polyphony from the late Middle-Ages and Renaissance will probably always be the purest form of organised sound invented by mankind). <br/>
+        Rather, I believe it's about time we stop deceiving brilliant and naïve young musicians who spend most of their lives dreaming of dreams that barely exist anymore in the real world. Instead, destroy the fences and invite classical music into our daily places with all the new and exciting players (alongside the old ones), actually supporting classical music in our daily lives: concerts in new venues with new music, new attire, new stages, new media, new values and, most importantly, new audiences (again, alongside the old ones).<br/>
+        <br/>
+        Musicians and artists need to be cherished and free, like everybody else, so they can keep being passionate about working and living off their craft.<br/>
+        <br/>
+        And boy, do we all need musicians and artists. Remember the lockdown?
+
+      </p>
     </div>
   )
 }
